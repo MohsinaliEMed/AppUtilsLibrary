c7680e79df94fe4b29a3a57111c42ed7d733ab2f
MohsinaliEMed <mohsinali.emedstore@gmail.com>
2021-03-19 18:18:22 +0530

    LogUtils debug condition

--- AppUtilsLibrary/src/main/java/com/mohsinali/appUtils/LogsUtils.kt
+++ AppUtilsLibrary/src/main/java/com/mohsinali/appUtils/LogsUtils.kt
@@ -1,6 +1,7 @@
 package com.mohsinali.appUtils
 
 import android.content.Context
+import android.content.pm.ApplicationInfo
 import android.util.Log
 import android.widget.Toast
 
@@ -8,8 +9,9 @@ import android.widget.Toast
 object LogsUtils {
     private const val DEFAULT_LOG_TAG = "@***@***@"
 
-    fun showError(string: String, buildType: String) {
-        if (buildType.equals("DEBUG", ignoreCase = true)) {
+    fun showError(string: String, context: Context) {
+        val isDebuggable = 0 != context.applicationInfo.flags and ApplicationInfo.FLAG_DEBUGGABLE
+        if (isDebuggable) {
             Log.e(DEFAULT_LOG_TAG, string)
         }
     }
